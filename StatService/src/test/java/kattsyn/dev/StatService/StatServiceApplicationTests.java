9f1beb0c82a5a0e2959485e998aa3293cee9731c
Kattsyn <vov4ikkat3@mail.ru>
2024-09-01 21:45:25 +0300

    init StatService project + stat-service.proto

---
+++ StatService/src/test/java/kattsyn/dev/StatService/StatServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kattsyn.dev.StatService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
