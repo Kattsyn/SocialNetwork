382acb2f6f0c2c8841d2703429ed8f35ac7b9e54
Kattsyn <vov4ikkat3@mail.ru>
2024-09-07 00:17:15 +0300

    changed GRPC_SERVER_ADDRESS to GRPC_CLIENT_POST-SERVICE_ADDRESS variable

--- StatService/src/test/java/kattsyn/dev/StatService/StatServiceApplicationTests.java
+++ StatService/src/test/java/kattsyn/dev/StatService/StatServiceApplicationTests.java
@@ -1,13 +1,32 @@
 package kattsyn.dev.StatService;
 
+import kattsyn.dev.StatService.dtos.EventCreationDto;
+import kattsyn.dev.StatService.entities.Event;
+import kattsyn.dev.StatService.enums.Events;
+import kattsyn.dev.StatService.services.EventService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
 
 @SpringBootTest
 class StatServiceApplicationTests {
 
-	@Test
+	@Autowired
+	public EventService eventService;
+
+
+    @Test
 	void contextLoads() {
+	}
+
+	@Test
+	void addEventTest() {
+		EventCreationDto event = new EventCreationDto(Events.EVENT_LIKE, 1L, 1L);
+		eventService.save(event);
+
+		System.out.println(eventService.findAll());
 	}
 
 }
